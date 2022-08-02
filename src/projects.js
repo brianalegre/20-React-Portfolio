844f4ca44ee5f53fcc2d4737e7875e3b9c5ac08f
Brian Alegre <brialegre@yahoo.com>
2022-08-02 14:46:41 -0700

    Updated projects.js with all projects to be listed

--- src/projects.js
+++ src/projects.js
@@ -8,8 +8,32 @@ const projects = [
     {
         projectName: "The Movie Library",
         projectImage: "./src/assets/projectImage/movieLibrary.png",
-        deployed: "https://brianalegre.github.io/Project1//",
-        repo: "https://github.com/brianalegre/Project1",
+        deployed: "https://brianalegre.github.io/Movie-Library/",
+        repo: "https://github.com/brianalegre/Movie-Library",
+    },
+    {
+        projectName: "Tech Blog",
+        projectImage: "TBD",
+        deployed: "https://alegre-techblog.herokuapp.com/",
+        repo: "https://github.com/brianalegre/14-MVC-Techblog",
+    },
+    {
+        projectName: "Weather Dashboard",
+        projectImage: "./src/assets/projectImage/weatherDashboard.png",
+        deployed: "https://brianalegre.github.io/06-Server-Side-APIs-Homework/",
+        repo: "https://github.com/brianalegre/06-Server-Side-APIs-Homework",
+    },
+    {
+        projectName: "Pokémon Quiz",
+        projectImage: "./src/assets/projectImage/pokemonQuiz.png",
+        deployed: "https://brianalegre.github.io/04-Web-APIs-Homework/",
+        repo: "https://github.com/brianalegre/04-Web-APIs-Homework",
+    },
+    {
+        projectName: "Password Generator",
+        projectImage: "./src/assets/projectImage/passwordGenerator.png",
+        deployed: "https://brianalegre.github.io/03-JavaScript-Homework/",
+        repo: "https://github.com/brianalegre/03-JavaScript-Homework",
     },
 ]
 
