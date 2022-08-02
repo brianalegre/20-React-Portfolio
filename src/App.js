93a1fb1a64900ee9443e4988b204e889eff61340
Brian Alegre <brialegre@yahoo.com>
2022-08-01 21:37:40 -0700

    Added small AboutMe Component.  Verified React is able to compile

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
 import React, { useState } from "react";
-import Header from "./components/Header";
-import Nav from "./components/Nav";
-import Page from "./components/Page";
-import Footer from "./components/Footer";
+import AboutMe from "./components/AboutMe";
+// import Nav from "./components/Nav";
+// import Page from "./components/Page";
+// import Footer from "./components/Footer";
 
 function App() {
-  const [pages] = useState([
-    {
-      name: "about me"
-    },
-    { name: "portfolio" },
-    { name: "contact" },
-    {
-      name: "resume"
-    }
-  ]);
-
-  const [currentPage, setCurrentPage] = useState(pages[0]);
 
   return (
     <div>
-      <Header>
-        <Nav
-          pages={pages}
-          setCurrentPage={setCurrentPage}
-          currentPage={currentPage}
-        ></Nav>
-      </Header>
-      <main>
-        <Page currentPage={currentPage}></Page>
-      </main>
-      <Footer />
+      <AboutMe />
     </div>
   );
 }
---
+++ src/components/AboutMe.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutMe() {
+    const message = `Hi, I'm Brian Alegre!`;
+
+    return (
+        <div className="container">
+            <h2>{message}</h2>
+        </div>
+    );
+}
+
+export default AboutMe;
