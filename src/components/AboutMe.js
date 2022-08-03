e312c6437c015bc05acdf5710901b674b99c79c4
Brian Alegre <brialegre@yahoo.com>
2022-08-02 22:48:51 -0700

    Updated AboutMe.js with introduction statement

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,19 @@
 import React from 'react';
 
+const message = `Hi, I'm Brian Alegre!`;
+const intro = `I love the fact that I can break things with Web Development, figuring out how it works, and
+also how to improve it, all while learning.
+I also love how Web Development is a great creative outlet for me. If its something I can
+imagine, I can bring it to life, and that iteslf if beautiful`
+
 function AboutMe() {
-    const message = `Hi, I'm Brian Alegre!`;
 
     return (
         <div className="text-3xl font-bold underline text-green-300">
             <h2>{message}</h2>
+            <p>
+                {intro}
+            </p>
         </div>
     );
 }
