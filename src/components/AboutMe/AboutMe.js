5dbd7b58c6cd015e7aed49106f36fc0422e606a1
Brian Alegre <brialegre@yahoo.com>
2022-08-10 09:32:17 -0700

    AboutMe Component - Fixed typo.  Skills Component - Reverted back to old method.  Still need to fix issue with scroll

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -3,7 +3,7 @@ import React from 'react';
 const intro = `I love the fact that I can break things with Web Development, figuring out how it works, and
 also how to improve it, all while learning.
 I also love how Web Development is a great creative outlet for me. If its something I can
-imagine, I can bring it to life, and that iteslf if beautiful`
+imagine, I can bring it to life, and that iteslf is beautiful`
 
 const selfie = '.././assets/logo/selfie.jpg'
 
--- src/components/Skills/Skills.js
+++ src/components/Skills/Skills.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Marquee from "react-fast-marquee";
+// import Marquee from "react-fast-marquee";
 
 import './Skills.css'
 // import { skillsImage } from './skillsImage'
 
 
-const skillsData = [
+const skills = [
     'HTML',
     'CSS',
     'Figma',
@@ -16,48 +16,48 @@ const skillsData = [
 ]
 
 function Skills() {
-    // return (
-    //     <section>
-    //         <h1>Skills</h1>
-    //         <p>Below are the skills I have learned thus far</p>
-
-    //         {skills.map((skillItems) => (
-    //             <div>
-    //                 <h3>{skillItems.skillName} </h3>
-    //             </div>
-    //         ))}
-    //     </section >
-    // );
-
     return (
-        <div className="skills">
-            <div className="skillsHeader">
-                <h2>Skills</h2>
-            </div>
-            <div className="skillsContainer">
-                <div className="skill--scroll">
-                    <Marquee
-                        gradient={false}
-                        speed={80}
-                        pauseOnHover={true}
-                        pauseOnClick={true}
-                        delay={0}
-                        play={true}
-                        direction="left"
-                    >
-                        {skillsData.map((skill, id) => (
-                            <div className="skill--box" key={id}>
-                                {/* <img src={skillsImage(skill)} alt={skill} /> */}
-                                <h3>
-                                    {skill}
-                                </h3>
-                            </div>
-                        ))}
-                    </Marquee>
+        <section>
+            <h1>Skills</h1>
+            <p>Below are the skills I have learned thus far</p>
+
+            {skills.map((skillItems) => (
+                <div>
+                    <h3>{skillItems.skillName} </h3>
                 </div>
-            </div>
-        </div>
-    )
+            ))}
+        </section >
+    );
+
+//     return (
+//         <div className="skills">
+//             <div className="skillsHeader">
+//                 <h2>Skills</h2>
+//             </div>
+//             <div className="skillsContainer">
+//                 <div className="skill--scroll">
+//                     <Marquee
+//                         gradient={false}
+//                         speed={80}
+//                         pauseOnHover={true}
+//                         pauseOnClick={true}
+//                         delay={0}
+//                         play={true}
+//                         direction="left"
+//                     >
+//                         {skillsData.map((skill, id) => (
+//                             <div className="skill--box" key={id}>
+//                                 {/* <img src={skillsImage(skill)} alt={skill} /> */}
+//                                 <h3>
+//                                     {skill}
+//                                 </h3>
+//                             </div>
+//                         ))}
+//                     </Marquee>
+//                 </div>
+//             </div>
+//         </div>
+//     )
 }
 
 export default Skills;
