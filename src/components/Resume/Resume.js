41dcd6b2eed4ef84056fe37f975ad6234c7cbabd
Brian Alegre <brialegre@yahoo.com>
2022-08-10 22:32:26 -0700

    Resume Component - need to debug error on loading Resume

--- src/components/Resume/Resume.js
+++ src/components/Resume/Resume.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import MyResume from './BrianAlegreResume.pdf'
-import { Document } from 'react-pdf'
+import React, { useState } from 'react';
+// import MyResume from './BrianAlegreResume.pdf'
+// import MyResume from '.././assets/BrianAlegreResume.pdf'
+import { Document, Page } from 'react-pdf';
 // https://www.npmjs.com/package/react-pdf
 
 function Resume() {
+    const [numPages, setNumPages] = useState(null);
+    const [pageNumber, setPageNumber] = useState(1);
+
+    function onDocumentLoadSuccess({ numPages }) {
+        setNumPages(numPages);
+    }
 
 
     return (
-        <div>
-            <Document file={MyResume}>
+        <div className="p-100 w-max">
+            <Document file='/BrianAlegreResume.pdf' onLoadSuccess={onDocumentLoadSuccess}>
+                <Page pageNumber={pageNumber} />
             </Document>
+            <p>
+                Page {pageNumber} of {numPages}
+            </p>
         </div>
     )
 }
