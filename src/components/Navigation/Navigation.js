bef8f32a9dfb39b4ed574f81710c2f348ba7b859
Brian Alegre <brialegre@yahoo.com>
2022-08-26 21:16:35 -0700

    Updated navbar to have hamburger menu

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,84 +1,188 @@
 // IMPORT
-import React from 'react';
+import React, { useState } from 'react';
 
-// GET PROPS FROM PARENT COMPONENT
-function Navbar({ currentPage, handlePageChange }) {
 
-    // NAVBAR ITEMS
-    const navbar = [
-        {
-            navName: "About Me",
-            navLink: "#aboutMe"
-        },
-        {
-            navName: "Portfolio",
-            navLink: "#portfolio"
-        },
-        {
-            navName: "Contact",
-            navLink: "#contact"
-        },
-        {
-            navName: "Resume",
-            navLink: "#resume"
-        }
-    ].map(navItems => ({ ...navItems, isActive: navItems.navName === currentPage }));
 
-    return (
 
-        <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
-            <div className="container flex flex-wrap justify-between items-center mx-auto">
-                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Brian Alegre</span>
-            <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
-                <span className="sr-only">Open main menu</span>
-                <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
-            </button>
-            <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    {/* MAP OUT NAVBAR */}
-                    {navbar.map(navItem => (
-                        <li
-                            key={navItem.navName}
-                            className={`${navItem.isActive ? "text-orange-500" : "text-gray-700 md:hover:text-orange-500"}` + " flex items-center mr-4"}
+// const navbar = [
+//   {
+//     navName: "About Me",
+//     navLink: "#aboutMe"
+//   },
+//   {
+//     navName: "Portfolio",
+//     navLink: "#portfolio"
+//   },
+//   {
+//     navName: "Contact",
+//     navLink: "#contact"
+//   },
+//   {
+//     navName: "Resume",
+//     navLink: "#resume"
+//   }
+// ]
 
-                            // ONCLICK HANDLE PAGE CHANGE
-                            onClick={() => handlePageChange(navItem.navName)}
-                        >
-                            {navItem.navName}
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </div>
-        </nav >
+// // GET PROPS FROM PARENT COMPONENT
+// function Navbar({ currentPage, handlePageChange }) {
 
-    );
-}
 
-export default Navbar;
 
-// <nav className="relative flex flex-wrap items-center justify-between px-2 py-3 bg-gray-300 mb-3">
-// <div className="container px-4 mx-auto flex flex-wrap items-center justify-between">
-//     <div className="w-full relative flex justify-between lg:w-auto  px-4 lg:static lg:block lg:justify-start">
-//         <p>
-//             Brian Alegre
-//         </p>
-//     </div>
-//     <div className="lg:flex flex-grow items-center">
-//         <ul className="flex flex-col lg:flex-row list-none ml-auto ">
+//   return (
+
+//     <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
+//       <div className="container flex flex-wrap justify-between items-center mx-auto">
+//         <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Brian Alegre</span>
+//         <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
+//           <span className="sr-only">Open main menu</span>
+//           <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+//         </button>
+//         <div className="hidden w-full md:block md:w-auto" id="navbar-default">
+//           <ul className="flex flex-col p-4 mt-4 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
 //             {/* MAP OUT NAVBAR */}
 //             {navbar.map(navItem => (
-//                 <li
-//                     key={navItem.navName}
-//                     className={`${navItem.isActive ? "text-orange-500" : "text-gray-700"}` + " flex items-center mr-4"}
+//               <li
+//                 key={navItem.navName}
+//                 className={`${navItem.isActive ? "text-orange-500" : "text-gray-700 md:hover:text-orange-500"}` + " flex items-center mr-4"}
 
-//                     // ONCLICK HANDLE PAGE CHANGE
-//                     onClick={() => handlePageChange(navItem.navName)}
-//                 >
-//                     {navItem.navName}
-//                 </li>
+//                 // ONCLICK HANDLE PAGE CHANGE
+//                 onClick={() => handlePageChange(navItem.navName)}
+//               >
+//                 {navItem.navName}
+//               </li>
 //             ))}
-//         </ul>
-//     </div>
-// </div>
-// </nav >
+//           </ul>
+//         </div>
+//       </div>
+//     </nav >
+
+//   );
+// }
+
+// export default Navbar;
+
+
+// HAMBURGER MENU
+
+export default function Navbar({ currentPage, handlePageChange }) {
+
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
+  // NAVBAR ITEMS
+  const navbar = [
+    {
+      navName: "About Me",
+      navLink: "#aboutMe"
+    },
+    {
+      navName: "Portfolio",
+      navLink: "#portfolio"
+    },
+    {
+      navName: "Contact",
+      navLink: "#contact"
+    },
+    {
+      navName: "Resume",
+      navLink: "#resume"
+    }
+  ].map(navItems => ({ ...navItems, isActive: navItems.navName === currentPage }));
+
+
+  return (
+    <div className="flex items-center justify-between bg-white border-gray-200 py-8 px-2.5 rounded text-sm">
+      {/* <a href="/">
+        <img src="https://designbygio.it/images/logo.png" alt="logo" />
+      </a> */}
+      <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Brian Alegre</span>
+      <nav>
+        <section className="MOBILE-MENU flex lg:hidden">
+          <div
+            className="HAMBURGER-ICON space-y-2"
+            onClick={() => setIsNavOpen((prev) => !prev)}
+          >
+            <span className="block h-0.5 w-8 animate-pulse bg-black"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-black"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-black"></span>
+          </div>
+
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+
+            <div
+              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)}
+            >
+              <svg
+                className="h-8 w-8 text-gray-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
+              {/* <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/about">About</a>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/portfolio">Portfolio</a>
+              </li>
+              <li className="border-b border-gray-400 my-8 uppercase">
+                <a href="/contact">Contact</a>
+              </li> */}
+              {navbar.map(navItem => (
+                <li
+                  key={navItem.navName}
+                  className={`${navItem.isActive ? "text-orange-500" : "text-gray-700 md:hover:text-orange-500"}` + " flex items-center mr-4"}
+
+                  // ONCLICK HANDLE PAGE CHANGE
+                  onClick={() => handlePageChange(navItem.navName)}
+                >
+                  {navItem.navName}
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
+
+        <ul className="DESKTOP-MENU hidden space-x-8 lg:flex">
+          {navbar.map(navItem => (
+            <li
+              key={navItem.navName}
+              className={`${navItem.isActive ? "text-orange-500" : "text-gray-700 md:hover:text-orange-500"}` + " flex items-center mr-4"}
+
+              // ONCLICK HANDLE PAGE CHANGE
+              onClick={() => handlePageChange(navItem.navName)}
+            >
+              {navItem.navName}
+            </li>
+          ))}
+        </ul>
+      </nav>
+      <style>{`
+      .hideMenuNav {
+        display: none;
+      }
+      .showMenuNav {
+        display: block;
+        position: absolute;
+        width: 100%;
+        height: 100vh;
+        top: 0;
+        left: 0;
+        background: white;
+        z-index: 10;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-items: center;
+      }
+    `}</style>
+    </div>
+  );
+}
